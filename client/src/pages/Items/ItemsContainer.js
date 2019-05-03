5bdc488248336fb40cf5e6939e285d1b74df7570
Andre Moura <andremarquesmoura@gmail.com>
2019-05-03 14:19:28 -0700

    Add context to Itemscontainer.js

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -3,6 +3,7 @@ import Items from './Items';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 import FullScreenLoader from '../../components/FullScreenLoader';
+import { ViewerContext } from '../../context/ViewerProvider';
 import { Query } from 'react-apollo';
 import {
   ALL_TAGS_QUERY,
@@ -14,13 +15,17 @@ import {
 class ItemsContainer extends Component {
   render() {
     return (
-      <Query query={ALL_ITEMS_QUERY}>
-        {({ loading, error, data }) => {
-          if (loading) return <FullScreenLoader inverted />;
-          if (error) return <p>{`Error! ${error.message}`}</p>;
-          return <Items classes={this.props.classes} items={data.items} />;
-        }}
-      </Query>
+      <ViewerContext.Consumer>
+        {({ loading, viewer }) => (
+          <Query query={ALL_ITEMS_QUERY} variables={{ filter: viewer.id }}>
+            {({ loading, error, data }) => {
+              if (loading) return <FullScreenLoader inverted />;
+              if (error) return <p>{`Error! ${error.message}`}</p>;
+              return <Items classes={this.props.classes} items={data.items} />;
+            }}
+          </Query>
+        )}
+      </ViewerContext.Consumer>
     );
   }
 }
