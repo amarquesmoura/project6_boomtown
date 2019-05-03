f823709e3042914c15fe04d3ab32c77c91a46077
Andre Moura <andremarquesmoura@gmail.com>
2019-05-03 15:51:58 -0700

    Add MainMenu

---
+++ client/src/components/MainMenu/styles.js
@@ -0,0 +1,14 @@
+const styles = theme => ({
+  root: {
+    display: 'flex'
+  },
+  buttonLink: {
+    textDecoration: 'none',
+    display: 'flex'
+  },
+  paper: {
+    marginRight: theme.spacing.unit * 2
+  }
+});
+
+export default styles;
