180540f2505ec0b7fc988e678c44718d64d47ef7
Andre Moura <andremarquesmoura@gmail.com>
2019-05-05 13:48:25 -0700

    Add styling to Items page (styles.js)

--- client/src/pages/Items/styles.js
+++ client/src/pages/Items/styles.js
@@ -1,3 +1,9 @@
-const styles = theme => ({});
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    background: theme.palette.secondary.main
+  }
+});
 
 export default styles;
